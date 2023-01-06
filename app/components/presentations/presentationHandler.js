b4f4de68c6234cd502448224c729f223b35456ec
Manh <k19weandweb@gmail.com>
2023-01-06 11:32:40 +0700

    handle participants list and chat box

--- app/components/presentations/presentationHandler.js
+++ app/components/presentations/presentationHandler.js
@@ -6,10 +6,10 @@ const presentationService = require("./presentationService");
 module.exports = (io, socket) => {
   const userJoinPresent = async (data) => {
     try {
-      console.log("data", data);
+      // console.log("data", data);
       const currentPresentationJson = await rediscl.get(data.code);
       const currentPresentation = JSON.parse(currentPresentationJson);
-      console.log("currentPresentation", currentPresentation);
+      // console.log("currentPresentation", currentPresentation);
 
       if (currentPresentation && data) {
         const presentation = await presentationService.getPresentationById(
@@ -20,7 +20,7 @@ module.exports = (io, socket) => {
         if (presentation) {
           if (presentation.status === 0) {
             //status === 0 means that the presentation is private
-            console.log("currentPresentation", currentPresentation);
+            // console.log("currentPresentation", currentPresentation);
             if (data.user.id && currentPresentation.groupId) {
               const isJoinedGroup = await groupService.isJoinedGroup(
                 parseInt(currentPresentation.groupId),
@@ -38,6 +38,29 @@ module.exports = (io, socket) => {
           // console.log("SERVER_SEND_JOIN_SUCCESS");
           socket.join(data.code);
           socket.emit("SERVER_SEND_JOIN_SUCCESS", data, currentPresentation);
+
+          //add user to list participants
+          const listParticipantsJson = await rediscl.get(
+            `presentation${currentPresentation.presentationId}_participants`
+          );
+          const listParticipants = JSON.parse(listParticipantsJson);
+          if (listParticipants) {
+            const existedUser = listParticipants.find(
+              (participant) => participant.id === data.user.id
+            );
+            if (!existedUser) {
+              const newListParticipants = listParticipants.concat({
+                ...data.user,
+              });
+              rediscl.set(
+                `presentation${currentPresentation.presentationId}_participants`,
+                JSON.stringify(newListParticipants)
+              );
+              io.sockets
+                .to(currentPresentation.host)
+                .emit("SERVER_SEND_LIST_PARTICIPANTS", newListParticipants);
+            }
+          }
         }
       } else {
         socket.emit("SERVER_SEND_JOIN_FAIL");
@@ -52,7 +75,7 @@ module.exports = (io, socket) => {
   };
 
   const startPresent = async (data) => {
-    const { code, presentationId, slideId, groupId } = data;
+    const { code, presentationId, slideId, groupId, user } = data;
     socket.join(code);
     socket.room = code;
     console.log("start", data);
@@ -65,14 +88,24 @@ module.exports = (io, socket) => {
         host: socket.id,
       })
     );
+    rediscl.set(
+      `presentation${presentationId}_participants`,
+      JSON.stringify([
+        {
+          ...user,
+        },
+      ])
+    );
     console.log("rediscl", await rediscl.get(code));
     // console.log(socket.adapter.rooms);
   };
 
   const endPresent = async (data) => {
     console.log("end present", data);
-    rediscl.del(data);
-    io.sockets.in(data).emit("PARTICIPANT_END_PRESENT");
+    rediscl.del(data.code);
+    rediscl.del(`presentation${data.presentationId}_participants`);
+    rediscl.del(`presentation${data.presentationId}_chatMessages`);
+    io.sockets.in(data.code).emit("PARTICIPANT_END_PRESENT");
     // console.log("rediscl", await rediscl.get(data));
     // console.log(socket.adapter.rooms);
   };
@@ -115,19 +148,62 @@ module.exports = (io, socket) => {
 
   const handleCreateChatMessage = async (data) => {
     try {
-      const { code, ...chatMessage } = data;
-      const newChatMessage = await presentationService.createChatMessage(
-        chatMessage
+      const { code, presentationId, ...chatInfo } = data;
+      // const newChatMessage = await presentationService.createChatMessage(
+      //   chatMessage
+      // );
+      // const userInfo = await userService.getUserById(chatMessage.userId);
+      // io.sockets.in(code).emit("SERVER_SEND_CHAT_MESSAGE", {
+      //   ...newChatMessage.dataValues,
+      //   avatar: userInfo.picture,
+      //   name: `${userInfo.firstName ?? ""} ${userInfo?.lastName ?? ""}`,
+      //   messages: [newChatMessage.content],
+      // });
+
+      // console.log("data", code, presentationId, chatInfo);
+
+      io.sockets.in(code).emit("SERVER_SEND_CHAT_MESSAGE", chatInfo);
+
+      const chatMessagesJson = await rediscl.get(
+        `presentation${presentationId}_chatMessages`
       );
-      const userInfo = await userService.getUserById(chatMessage.userId);
-      io.sockets.in(code).emit("SERVER_SEND_CHAT_MESSAGE", {
-        ...newChatMessage.dataValues,
-        avatar: userInfo.picture,
-        name: `${userInfo.firstName ?? ""} ${userInfo?.lastName ?? ""}`,
-        messages: [newChatMessage.content],
-      });
+      if (chatMessagesJson) {
+        const chatMessages = JSON.parse(chatMessagesJson);
+        const newChatMessages = chatMessages.concat(chatInfo);
+        rediscl.set(
+          `presentation${presentationId}_chatMessages`,
+          JSON.stringify(newChatMessages)
+        );
+      } else {
+        rediscl.set(
+          `presentation${presentationId}_chatMessages`,
+          JSON.stringify([chatInfo])
+        );
+      }
     } catch (err) {
       console.log(err);
+    }
+  };
+
+  const handleResponseChatMessage = async (data) => {
+    const { code, presentationId } = data;
+    const chatMessagesJson = await rediscl.get(
+      `presentation${presentationId}_chatMessages`
+    );
+    if (chatMessagesJson) {
+      const chatMessages = JSON.parse(chatMessagesJson);
+      socket.emit("SERVER_SEND_LIST_MESSAGES", chatMessages);
+    }
+  };
+
+  const handleResponseParticipantsList = async (data) => {
+    const { code, presentationId } = data;
+    const participantsJson = await rediscl.get(
+      `presentation${presentationId}_participants`
+    );
+    if (participantsJson) {
+      const participants = JSON.parse(participantsJson);
+      socket.emit("SERVER_SEND_LIST_PARTICIPANTS", participants);
     }
   };
 
@@ -137,4 +213,6 @@ module.exports = (io, socket) => {
   socket.on("HOST_MOVE_TO_SLIDE", moveToSlide);
   socket.on("PARTICIPANT_SEND_INCREASE_VOTE", increaseVote);
   socket.on("PARTICIPANT_SEND_MESSAGE", handleCreateChatMessage);
+  socket.on("CLIENT_GET_LIST_MESSAGES", handleResponseChatMessage);
+  socket.on("CLIENT_GET_LIST_PARTICIPANTS", handleResponseParticipantsList);
 };
