5a09d15d7ec385db9cef849b277ef168cc3db74c
Manh <k19weandweb@gmail.com>
2023-01-10 18:23:28 +0700

    Fix bug deploy failed

--- app/components/presentations/presentationRouter.js
+++ app/components/presentations/presentationRouter.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const PresentationController = require("./PresentationController");
+const PresentationController = require("./presentationController");
 const groupMiddleware = require("../groups/groupMiddleware");
 
 // Create new presentation
